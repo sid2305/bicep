b2391ed36ef1d8f80ed00e43bd819588221ed28e
Shenglong Li <shenglol@microsoft.com>
2023-10-10 18:09:57 -0700

    Add Codicon

--- src/vscode-bicep/ui/packages/icons/src/AzureIcon/AzureIcon.tsx
+++ src/vscode-bicep/ui/packages/icons/src/AzureIcon/AzureIcon.tsx
@@ -1,23 +1,32 @@
-import { useAzureSvg } from "./use-azure-svg";
 import styled from "styled-components";
+import { useAzureSvg } from "./use-azure-svg";
 
 interface AzureIconProps {
+  /**
+   * Fully qualified Azure resource type, e.g. Microsoft.Compute/virtualMachines
+   */
   resourceType: string;
+  /**
+   * Size of the icon in pixels
+   */
   size: number;
 }
 
-const $SvgIcon = styled.div<{ $size: number; }>`
+const $AzureIcon = styled.div<{ $size: number }>`
   width: ${(props) => props.$size}px;
   height: ${(props) => props.$size}px;
   text-align: center;
 `;
 
+/**
+ * UI component for rendering an Azure resource type icon
+ */
 export function AzureIcon({ resourceType, size }: AzureIconProps) {
   const { loading, AzureSvg } = useAzureSvg(resourceType);
 
   return (
-    <$SvgIcon $size={size}>
+    <$AzureIcon $size={size}>
       {!loading && <AzureSvg width="100%" height="100%" />}
-    </$SvgIcon>
+    </$AzureIcon>
   );
 }
