7c9538e3d752db55a15cab0026d3a8be97a08821
Shenglong Li <shenglol@microsoft.com>
2023-10-11 21:19:23 -0700

    Build optimization

--- src/vscode-bicep/ui/packages/icons/src/index.ts
+++ src/vscode-bicep/ui/packages/icons/src/index.ts
@@ -1,1 +1,2 @@
-export * from "./AzureIcon";
+export * from "./azure-icon";
+export * from "./codicon";
--- src/vscode-bicep/ui/packages/icons/vite.config.ts
+++ src/vscode-bicep/ui/packages/icons/vite.config.ts
@@ -1,8 +1,9 @@
-import path from "node:path";
+// import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import svgr from "vite-plugin-svgr";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,24 +11,30 @@ export default defineConfig({
     react(),
     svgr(),
     dts({
+      exclude: ["**/*.stories.ts", "**/*.stories.tsx", "**/use-*.ts"],
       insertTypesEntry: true,
     }),
-  ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+    // libInjectCss(),
+    libInjectCss({
       formats: ["es"],
-    },
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "react/jsx-runtime",
-        "styled-components",
-      ],
-      output: {
-        entryFileNames: '[name].js',
-      }
-    },
-  },
+      entry: {
+        index: "src/index.ts",
+        "azure-icon": "src/azure-icon/index.ts",
+        codicon: "src/codicon/index.ts",
+      },
+      rollupOptions: {
+        external: [
+          "react",
+          "react-dom",
+          "react/jsx-runtime",
+          "styled-components",
+        ],
+        output: {
+          entryFileNames: "[name].js",
+          chunkFileNames: "chunks/[name].[hash].js",
+          assetFileNames: "assets/[name][extname]",
+        },
+      },
+    }),
+  ],
 });
