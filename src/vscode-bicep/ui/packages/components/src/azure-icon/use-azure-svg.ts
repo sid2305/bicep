17eb60be9859bd34e64a36649ff0c2ada8591722
shenglol <shenglol@microsoft.com>
2023-10-11 22:42:15 -0700

    Update folder structure

--- src/vscode-bicep/ui/packages/components/src/azure-icon/use-azure-svg.ts
+++ src/vscode-bicep/ui/packages/components/src/azure-icon/use-azure-svg.ts
@@ -1,31 +1,35 @@
 import { useEffect, useRef, useState } from "react";
-import ResourceSvg from "../assets/icons/azure/custom/resource.svg?react";
+import ResourceSvg from "../../assets/azure-architecture-icons/custom/resource.svg?react";
 
 type SvgComponent = typeof ResourceSvg;
 
+const svgPathsByResourceType: Record<string, string> = {
+  // Microsoft.Compute
+  "microsoft.compute/virtualmachines":
+    "compute/10021-icon-service-Virtual-Machine",
+  "microsoft.compute/virtualmachinescalesets":
+    "compute/10034-icon-service-VM-Scale-Sets",
+
+  // Microsoft.Network
+  "microsoft.network/networkinterfaces":
+    "networking/10080-icon-service-Network-Interfaces",
+  "microsoft.network/loadbalancers":
+    "networking/10062-icon-service-Load-Balancers",
+
+  // Microsoft.Web
+  "microsoft.web/serverfarms":
+    "app-services/00046-icon-service-App-Service-Plans",
+  "microsoft.web/sites": "app-services/10035-icon-service-App-Services",
+};
+
 const svgImportsByPath = import.meta.glob<SvgComponent>(
-  `../assets/icons/azure/**/*.svg`,
+  `../../assets/azure-architecture-icons/**/*.svg`,
   {
     query: "react",
     import: "default",
   },
 );
 
-const svgPathsByResourceType: Record<string, string> = {
-  "microsoft.compute/virtualmachines":
-    "compute/10021-icon-service-Virtual-Machine",
-  "microsoft.compute/virtualmachinescalesets":
-    "compute/10034-icon-service-VM-Scale-Sets",
-
-  "microsoft.network/networkinterfaces":
-    "networking/10080-icon-service-Network-Interfaces",
-  "microsoft.network/loadbalancers":
-    "networking/10062-icon-service-Load-Balancers",
-
-  "microsoft.web/serverfarms":
-    "app-services/00046-icon-service-App-Service-Plans",
-  "microsoft.web/sites": "app-services/10035-icon-service-App-Services",
-};
 
 async function tryImportAzureSvg(
   resourceType: string,
@@ -34,7 +38,7 @@ async function tryImportAzureSvg(
 
   if (resourceType in svgPathsByResourceType) {
     const svgPath = svgPathsByResourceType[resourceType];
-    const svgImport = svgImportsByPath[`../assets/icons/azure/${svgPath}.svg`];
+    const svgImport = svgImportsByPath[`../../assets/azure-architecture-icons/${svgPath}.svg`];
 
     return svgImport?.();
   }
